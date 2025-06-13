03ce7d097810a89109d44ab24dfd5067b57d7fcd
Liko20 <hazarika857@gmail.com>
2025-06-14 04:47:37 +0530

    added index

--- models/syncDevice/model.js
+++ models/syncDevice/model.js
@@ -5,7 +5,7 @@ const syncDeviceSchema = new mongoose.Schema(
     device_id: { type: String, index: true, required: true },
     timestamp: { type: Date, default: Date.now },
     total_files_synced: { type: Number, default: 0 },
-    total_errors: { type: Number, default: 0 },
+    total_errors: { type: Number, index: true, default: 0 },
     internet_speed: { type: Number, default: 0 },
   },
   { timestamps: true }
